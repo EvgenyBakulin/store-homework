c69947ac47e271fb31ac1b8931b349f6fd7190cd
Евгений <ugend84@yandex.ru>
2022-04-21 10:40:16 +0300

    Initial commit

---
+++ src/main/java/pro/sky/storehomework/StoreHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.storehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/storehomework/StoreHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.storehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
