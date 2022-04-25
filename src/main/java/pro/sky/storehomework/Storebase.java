6d29f8cdf4dbb2140a8d3dd3b72bccef040606eb
Евгений <ugend84@yandex.ru>
2022-04-25 10:55:09 +0300

    Домашнее задание по Спринг, с магазином и корзиной заказов - поправленное

--- src/main/java/pro/sky/storehomework/Storebase.java
+++ src/main/java/pro/sky/storehomework/Storebase.java
@@ -1,8 +1,10 @@
 package pro.sky.storehomework;
 
+import org.springframework.web.context.annotation.SessionScope;
+
 import java.util.HashMap;
 import java.util.Map;
-
+@SessionScope
 public class Storebase {
     private Map<Integer, String> storebase = Map.of(1, "Milk", 2, "Bread", 3, "Eggs",
             4, "Meat", 5, "Fruits", 6, "Bear");
